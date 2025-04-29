9cd941c5bed184d21577b96062ab0b899d8007a2
ngt22 <mail.ngt@gmail.com>
2025-04-29 14:07:02 +0900

    add definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+// ここに追加
+declare const __APP_NAME__: string;
+declare const __APP_VERSION__: string;
+
+// 他の型定義があればそのまま残す
+interface ImportMetaEnv {
+  // readonly VITE_APP_TITLE: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
